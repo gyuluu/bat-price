fe298cf305de68aa65ebca751f3963e014dfe0b5
Vlad Anghel <vlad.anghel1@gmail.com>
2019-11-29 23:16:13 +0100

    add price chart

--- src/Price.js
+++ src/Price.js
@@ -1,22 +1,59 @@
 import React,{ useState, useEffect }  from "react";
 import axios from "axios";
+import { Line } from 'react-chartjs-2';
 
 export default function Price() {
     const [price, setPrice] = useState(0);
+    const [arr, setArr] = useState([]);
+    const [ts, setTs] = useState([]);
+    const data = {
+        labels: [...ts],
+        datasets: [
+          {
+            label: 'BAT Price (USD)',
+            data: [...arr],
+            fill: false,          
+            borderColor: 'red' 
+          }
+        ]
+      }
+      const options = {
+        maintainAspectRatio: false	
+      }
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        const bat = () => {
             axios.get("https://api.cryptonator.com/api/ticker/bat-usd").then(
             resp => {
                 setPrice(resp.data.ticker.price);
+                if (parseFloat(resp.data.ticker.price) !== arr[arr.length - 1]) {
+                    let x = parseFloat(resp.data.ticker.price);
+                    let y = parseInt(resp.data.timestamp);
+                    let date = new Date(y*1000);
+                    let hours = date.getHours();
+                    let minutes = "0" + date.getMinutes();
+                    let seconds = "0" + date.getSeconds();
+                    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                    setArr(arr => [...arr, x]);
+                    setTs(ts => [...ts, formattedTime]); 
+                }
             });
-        }, 500);
+        }
+
+        bat();
+        const interval = setInterval(bat, 30000);
         return () => clearInterval(interval);     
     }, []);
 
     return (
-        <div className="price-wrapper">
-            <div className="price">${price}</div>
-        </div>  
+        <div>
+            <div className="w">
+                <div className="price">${price}</div>
+            </div>  
+            <div className="canvas-container">
+                <Line data={data} options={options}/>
+            </div>
+        </div>
+        
     )
 }
